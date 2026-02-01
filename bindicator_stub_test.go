a9194b7bbb49ba9a4cfbfc6df5c652e15a8b0372
Nick Gregory <nick.gregory@openenterprise.co.uk>
2026-02-01 13:00:36 +0000

    fix action

--- bindicator_stub_test.go
+++ bindicator_stub_test.go
@@ -4,42 +4,6 @@ package main
 
 import "time"
 
-// BinType represents the type of bin collection
-type BinType uint8
-
-const (
-	BinUnknown BinType = iota
-	BinGreen
-	BinBlack
-	BinBrown
-)
-
-// BinJob represents a scheduled bin collection
-type BinJob struct {
-	Year  uint16
-	Month uint8
-	Day   uint8
-	Bin   BinType
-}
-
-// Pre-allocated storage for bin jobs (avoids heap allocation)
-const maxJobs = 15
-
-var (
-	jobStorage [maxJobs]BinJob
-	jobCount   int
-)
-
-// getJobs returns the current job storage slice
-func getJobs() []BinJob {
-	return jobStorage[:jobCount]
-}
-
-// clearJobs resets the job count
-func clearJobs() {
-	jobCount = 0
-}
-
 // LED state storage (for testing)
 var ledState struct {
 	green bool
